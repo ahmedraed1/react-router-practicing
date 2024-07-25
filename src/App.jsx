6bd3dee84fb8195066f3b9eb7278932cd01b8207
Ahmed Raed <ahmedraed101@yahoo.com>
2024-07-25 18:34:42 +0300

    router-simple-example

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-// import { useState } from 'react';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./view/about";
+import Header from "./components/header";
+import Home from "./view/home";
+import NotFoundPage from "./view/notFoundPage";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/view/notFoundPage.jsx
@@ -0,0 +1,3 @@
+export default function NotFoundPage() {
+  return <h1 className="text-red-400 m-5">Sorry, This Page not Found :(</h1>;
+}
